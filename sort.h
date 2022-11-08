c7c9862c0ffbafd028264d82b137ca8668c29a89
sumutoni <steumutoni@gmail.com>
2022-11-08 12:58:53 -0800

    header file

--- sort.h
+++ sort.h
@@ -23,4 +23,6 @@ void print_array(const int *array, size_t size);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 int check_sorted(int *array, size_t size);
+int check_sorted_l(listint_t **list);
+void selection_sort(int *array, size_t size);
 #endif
