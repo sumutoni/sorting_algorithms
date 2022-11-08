101224e4d64c4471a9b0e3f3064276ebb014d0dc
sumutoni <steumutoni@gmail.com>
2022-11-08 13:07:43 -0800

    header file & big Onotation of quick sort

--- sort.h
+++ sort.h
@@ -25,4 +25,5 @@ void insertion_sort_list(listint_t **list);
 int check_sorted(int *array, size_t size);
 int check_sorted_l(listint_t **list);
 void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
 #endif
